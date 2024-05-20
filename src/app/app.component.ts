54f7e174ac58c5eeaf28e6d2f250a1a7240d8577
Michael Tarleton <michael.tarleton@me.com>
2024-05-19 20:40:15 -0500

    Added the comment box compenent to the App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AtCommentBoxComponent } from './at-comment-box/at-comment-box.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AtCommentBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
