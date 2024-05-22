8a4f85a36a2619a7222ba92125af58f7bb7d468f
Michael Tarleton <michael.tarleton@me.com>
2024-05-22 10:19:04 -0500

    [WIP] Temp stash

--- src/app/at-comment-box/at-comment-box.component.ts
+++ src/app/at-comment-box/at-comment-box.component.ts
@@ -10,6 +10,7 @@ import { AtDropdownComponent } from '../at-dropdown/at-dropdown.component';
   styleUrl: './at-comment-box.component.scss'
 })
 export class AtCommentBoxComponent {
+
   @ViewChild('comment', { read: ViewContainerRef }) comment: ViewContainerRef | undefined
   componentRefs: ComponentRef<any>[] = []
 
@@ -18,35 +19,59 @@ export class AtCommentBoxComponent {
   }) dropdownComponent: AtDropdownComponent | undefined
 
   public searchString: string = ''
+  public isFocus: boolean = false
+
+  private regex: RegExp = new RegExp(/[a-zA-Z0-9()\[\] -]/,'i')
+
   type($event: any) {
-    this.searchString += $event.key
+    const { key, keyCode, shiftKey } = $event
+    const atKeyCode: number = 50
+    const backspaceKeyCode: number = 8
+
+    console.log('type', $event.key, $event.which, $event.keyCode, $event)
+
+    // We should start looking up the list of names in the service
+    if(shiftKey && keyCode === atKeyCode) {
+      this.dropdownComponent?.focusInput()
+      this.searchString = ''
+    }
+
+    // If the key is backspace, remove the last character
+    // Else if the key is a single character (exclude the "ins", "home", etc. buttons)
+    // Then add to the search string
+    if(keyCode === backspaceKeyCode) {
+      this.searchString = this.searchString.slice(0, this.searchString.length-1)
+    } else if(key.length === 1 && this.regex.test(key)) {
+        this.searchString += $event.key
+    }
+
+    // Move the cursor to the end of the text box
     this.moveCursorToEnd($event.target)
     this.dropdownComponent
   }
-  isFocus: boolean = false
+
   onBlur() {
     this.isFocus = false
   }
+
   onFocus($event: any) {
     this.isFocus = true
     this.moveCursorToEnd($event.target)
   }
 
   remove($event: Event) {
-    console.log($event, $event.target)
+    console.log('remove', $event, $event.target)
     const factory = this.comment!.createComponent(AtUsernameComponent)
     const instance = factory?.instance as AtUsernameComponent
-    // instance.someproperty = 'somevalue'
 
     this.componentRefs.push(factory)
   }
 
   search($event: any) {
-    console.log($event, $event.target)
+    console.log('search', $event, $event.target)
     const factory = this.comment!.createComponent(AtUsernameComponent)
     const instance = factory?.instance as AtUsernameComponent
-    // instance.someproperty = 'somevalue'
-
+    instance.searchString = this.searchString
     this.componentRefs.push(factory)
   }
 
